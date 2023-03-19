8367419d44c768dfbe7af016b6cabf395a36e363
Tarun Singhania <37648118+TarunSinghania@users.noreply.github.com>
2023-03-19 21:17:50 +0530

    First branch all changes (#3)
    * commit 6 branch1
    
    * commit 7
    
    * commit 7
    
    * commit 9 aftermerge
    
    * commit 10 aftermerge
    
    * rebased
    
    * commit 12 afterrebase
    
    * commit 13 afterrebasesecond
    
    * commit 13 afterrebasesecond

--- branch1file.py
+++ branch1file.py
@@ -1,1 +1,10 @@
 #commit 5 branch1commit1
+#commit 6 branch1commit2
+#commit 7 branch1commit3
+#commit 8 branch1commit
+#commit 9 aftermerge
+#commit 10 aftermergesecond
+#commit 11 aftermergethird
+#commit 12 afterrebase
+#commit 13 afterrebasesecond
+#commit 14 afterrebasethird
